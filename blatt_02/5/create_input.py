d034461e4b6e17c76e84600a026bf0f8f8eeb8bc
Lennart Braun <lenerd@posteo.de>
2015-05-05 00:34:55 +0200

    create input script cleaned

--- blatt_02/5/create_input.py
+++ blatt_02/5/create_input.py
@@ -3,13 +3,6 @@ import random
 import sys
 
 
-def create_vector(path, size):
-    with open(path, 'w') as f:
-        f.write('{}\n'.format(size))
-        for i in range(size):
-            f.write('{}\n'.format(random.randint(0, 10000)))
-
-
 def create_matrix(path, rows, cols):
     with open(path, 'w') as f:
         f.write('{} {}\n'.format(rows, cols))
@@ -20,28 +13,15 @@ def create_matrix(path, rows, cols):
 
 
 def usage():
-    print("python create_input.py matrix 1337 42 filename")
-    print("python create_input.py vector 1337 filename")
+    print("python create_input.py 1337 42 filename")
 
 
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 4:
         usage()
         return
-    if sys.argv[1] == 'matrix':
-        if len(sys.argv) < 5:
-            usage()
-            return
-        else:
-            create_matrix(sys.argv[4], int(sys.argv[2]), int(sys.argv[3]))
-    elif sys.argv[1] == 'vector':
-        if len(sys.argv) < 4:
-            usage()
-            return
-        else:
-            create_vector(sys.argv[3], int(sys.argv[2]))
     else:
-        usage()
+        create_matrix(sys.argv[3], int(sys.argv[1]), int(sys.argv[2]))
 
 
 if __name__ == '__main__':
