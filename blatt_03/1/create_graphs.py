366725093d8cbd15f30d6160d01b02b24e7b3acb
Lennart Braun <lenerd@posteo.de>
2015-05-11 20:59:54 +0200

    speedup analysis

--- blatt_03/1/create_graphs.py
+++ blatt_03/1/create_graphs.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
+
+
+pp = PdfPages('speedup.pdf')
 
 
 def create_speedup_graph(data):
@@ -10,9 +14,14 @@ def create_speedup_graph(data):
     def speedup(time):
         return base_time / time
 
-    print([speedup(t) for t in data[:, 1]])
+    plt.title("Speedup")
     plt.plot(data[:, 0], [speedup(t) for t in data[:, 1]], 'ro')
-    plt.show()
+    plt.axis((0, 13, 0, 5))
+    plt.xlabel('Processes')
+    plt.ylabel('Speedup')
+    # plt.show()
+    pp.savefig()
+    plt.close()
 
 
 def create_efficiency_graph(data):
@@ -21,9 +30,14 @@ def create_efficiency_graph(data):
     def efficiency(proc, time):
         return base_time / time / proc
 
-    print([efficiency(p, t) for p, t in data])
+    plt.title("Efficiency")
     plt.plot(data[:, 0], [efficiency(p, t) for p, t in data], 'bo')
-    plt.show()
+    plt.axis((0, 13, 0, 1.2))
+    plt.xlabel('Processes')
+    plt.ylabel('Efficiency')
+    # plt.show()
+    pp.savefig()
+    plt.close()
 
 
 def main():
@@ -33,9 +47,9 @@ def main():
     data = np.zeros(shape)
     data[:, 0] = measurements[:, 0]
     data[:, 1] = sum(measurements[:, m] for m in range(1, num_m + 1)) / num_m
-    print(data)
     create_speedup_graph(data)
     create_efficiency_graph(data)
+    pp.close()
 
 
 if __name__ == '__main__':
