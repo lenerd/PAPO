48af98d485025d8428076faa30821eeb045317c9
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-05-18 16:54:48 +0200

    posix def

--- blatt_04/1/time_difference.h
+++ blatt_04/1/time_difference.h
@@ -1,6 +1,8 @@
 #ifndef TIME_DIFFERENCE_H
 #define TIME_DIFFERENCE_H
 
+#define _POSIX_C_SOURCE 199309L
+
 #include <time.h>
 
 void time_difference (struct timespec* src_a, struct timespec* src_b, struct timespec* dst);
